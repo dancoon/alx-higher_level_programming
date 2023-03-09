c5772653011ddaac4c86abe57e709db445b00b9c
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-03-09 08:50:37 -0800

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
-
-    n = len(sys.argv)
-    if n != 4:
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    op = sys.argv[2]
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    if op == '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op == '/':
-        print("{} {} {} = {}".format(div(a, b)))
+    op = sys.argv[2]
+    if (op == '+'):
+        ans = add(a, b)
+    elif (op == '-'):
+        ans = sub(a, b)
+    elif (op == '*'):
+        ans = mul(a, b)
+    elif (op == '/'):
+        ans = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+    print("{} {} {} = {}".format(a, op, b, ans))
