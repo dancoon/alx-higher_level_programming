e2af7d614dc639681fc60a55807d33b068b4100f
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-03-27 02:35:53 -0700

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        [print(my_list[i], end="") for i in range(x)]
-        count += 1
+        for i in range(x):
+            print(my_list[i], end=""
+            count += 1
     except IndexError:
         pass
     print()
