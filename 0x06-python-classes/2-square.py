1ba3cf75fbccabcec9ae3d76c194314274797f8d
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-03-28 03:33:40 -0700

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/pythons3
+"""defines a class Square"""
+
+
 class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, s=0):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
         if not type(s) is int:
             raise TypeError("size must be an integer")
         if s < 0:
