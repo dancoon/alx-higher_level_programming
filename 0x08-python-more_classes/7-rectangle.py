200a37dd61c429d08f3115025069226493594b02
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-04-03 06:50:49 -0700

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -76,7 +76,7 @@ class Rectangle:
         """
         s = ""
         if self.__width != 0 and self.__height != 0:
-            s += "\n".join("#" * self.__width for i in range(self.__height))
+            s += "\n".join(str(self.print_symbol) * self.__width for i in range(self.__height))
         return s
 
     def __repr__(self):
