52728c975244d84c7a8741c844fac846649ef41e
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-06-12 19:55:45 +0300

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-const arg1 = process.argv[2]
-const arg2 = process.argv[3]
-console.log(arg1 + " is " + arg2)
+const { argv } = require('process');
+const arg1 = argv[2];
+const arg2 = argv[3];
+console.log(arg1 + " is " + arg2);
