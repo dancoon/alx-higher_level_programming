7e12ca56ed365dd60a910cf986dd4a2c39803e49
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-06-12 20:30:03 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const argsCount = process.argv.length - 2;
-if (argsCount === 0) {
-  console.log("No argument");
-} else if (argsCount === 1) {
-  console.log("Argument found");
+const nargs = process.argv.length;
+if (nargs === 2) {
+  console.log('No argument');
+} else if (nargs === 3) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
