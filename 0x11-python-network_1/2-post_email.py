2e31322246f53db914f54bd2f23a271dc4230caa
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-06-30 12:40:20 +0300

    python network 1

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+import urllib
+import sys
+
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({"email": sys.argv[2]}).encode("utf-8")
+    with urllib.request.urlopen(sys.argv[1], data) as response:
+        print(response.read().decode("utf-8"))
