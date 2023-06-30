9f357fb0762c8255b67f64fa86eed784b9eb5603
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-06-30 02:53:31 -0700

    Python network 1 task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays
-the body of the response (decoded in utf-8)."""
+"""
+Takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+
+
 import urllib
 import sys
-
 
 if __name__ == "__main__":
     try:
