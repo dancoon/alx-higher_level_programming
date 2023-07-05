9725c216a2ac1d95a718093aee5d65a70e6bdbef
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-07-05 09:38:37 +0300

    done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,7 +2,8 @@
 """Takes in a URL and an email address, sends a POST request
 to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)"""
-import urllib
+import urllib.request
+import urllib.parse
 import sys
 
 
