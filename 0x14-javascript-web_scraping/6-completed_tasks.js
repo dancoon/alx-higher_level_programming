28ca0d0c4cd2230c2a1671a4b7ab1c3a90ae82bc
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-07-25 19:11:08 +0300

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Script that computes the number of tasks completed by user id.
-const request = require("request");
-const process = require("process");
+const request = require('request');
+const process = require('process');
 const url = process.argv[2];
 request(url, (err, response, body) => {
   if (err) console.log(err);
